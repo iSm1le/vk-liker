bb87f1588bafb82f0b1c566d34c8cd8a56fc816d
Mikhail K. (iSm1le) <ism1le@xaked.com>
2018-11-24 01:53:42 +0200

    Updated links

--- latest.js
+++ latest.js
@@ -3,10 +3,10 @@
 // @namespace    https://xaked.com/
 // @version      0.0.2
 // @description  Likes all finded wall posts on current page
-// @updateURL    https://github.com/iSm1le/vk-liker/blob/master/latest.js
-// @downloadURL  https://github.com/iSm1le/vk-liker/blob/master/index.js
-// @icon         https://github.com/iSm1le/vk-liker/blob/master/logo-48x48.png
-// @icon64       https://github.com/iSm1le/vk-liker/blob/master/logo-64x64.png
+// @updateURL    https://raw.githubusercontent.com/iSm1le/vk-liker/master/latest.js
+// @downloadURL  https://raw.githubusercontent.com/iSm1le/vk-liker/master/index.js
+// @icon         https://raw.githubusercontent.com/iSm1le/vk-liker/master/logo-48x48.png
+// @icon64       https://raw.githubusercontent.com/iSm1le/vk-liker/master/logo-64x64.png
 // @author       iSm1le
 // @copyright    2018 iSm1le
 // @match        *://vk.com/*
